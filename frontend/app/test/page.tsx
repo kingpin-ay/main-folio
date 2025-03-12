017b9492003ea863dce7a8bbc2a6f0fd2739fb13
kingpin-ay <ayushmondal001@gmail.com>
2025-03-12 19:47:01 +0530

    feat: added the fix for the hono tester

---
+++ frontend/app/test/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function page() {
+  const checkCall = async () => {
+    const result = await fetch(
+      "https://2r5ixl6xr9.execute-api.ap-south-1.amazonaws.com/default/health",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log("response check for the hono-lambda -> ", result.body, result);
+  };
+
+  React.useEffect(() => {
+    checkCall();
+  }, []);
+  return <div>page</div>;
+}
+
+export default page;
