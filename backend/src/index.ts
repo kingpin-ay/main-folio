6915abe88e712f507c8b0fedec70d85cb3b2b57b
kingpin-ay <ayushmondal001@gmail.com>
2025-03-13 13:10:44 +0530

    fix: removed the cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,6 @@ import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
 
 const app = new Hono();
-const header = {
-  "Access-Control-Allow-Origin": "https://main.d3s09nbx3p1m4t.amplifyapp.com",
-  "Access-Control-Allow-Headers": "Content-Type,Authorization",
-  "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-};
 
 app.get("/", (c) => {
   return c.text("Hello hono!", 200, header);
