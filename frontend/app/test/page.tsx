b6931b4105927f4c56540d352d07173cb35aeb60
kingpin-ay <ayushmondal001@gmail.com>
2025-03-15 23:55:31 +0530

    feat: changed the url

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -4,26 +4,12 @@ import { useEffect } from "react";
 function Test() {
   const checkCall = async () => {
     const result = await fetch(
-      "https://p04ytf3xrh.execute-api.ap-south-1.amazonaws.com/default",
+      "https://p04ytf3xrh.execute-api.ap-south-1.amazonaws.com/default/",
       {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-        mode: "cors", // Ensure CORS is enabled
-      }
-    );
-    console.log("response check for the hono-lambda -> ", result.body, result);
-  };
-  const checkCallmain = async () => {
-    const result = await fetch(
-      "https://p04ytf3xrh.execute-api.ap-south-1.amazonaws.com/default/check",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        mode: "cors", // Ensure CORS is enabled
       }
     );
     console.log("response check for the hono-lambda -> ", result.body, result);
@@ -31,7 +17,6 @@ function Test() {
 
   useEffect(() => {
     checkCall();
-    checkCallmain();
   }, []);
   return <div>page</div>;
 }
