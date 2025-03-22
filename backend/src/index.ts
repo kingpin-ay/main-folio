877d6442451bd186035a825e3e926b761f42f032
kingpin-ay <ayushmondal001@gmail.com>
2025-03-23 01:09:34 +0530

    feat: test page checking

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { handle, LambdaEvent } from "hono/aws-lambda";
 import { cors } from "hono/cors";
+// import { serve } from "@hono/node-server";
 
 type Bindings = {
   event: LambdaEvent;
@@ -8,7 +9,7 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>().basePath("/default");
 
-app.use("*", cors());
+app.use("https://main.d3s09nbx3p1m4t.amplifyapp.com/", cors());
 
 app.get("/", (c) => {
   return c.text(`Hello hono! ${c.req.path}`, 200);
@@ -17,5 +18,7 @@ app.get("/", (c) => {
 app.get("/update", (c) => {
   return c.text(`Hello hono starting point! ${c.req.path}`, 200);
 });
+
+// serve({ port: 3001, fetch: app.fetch });
 
 export const handler = handle(app);
