957103643907ca5c6bc2880ec5c3aede324d7890
kingpin-ay <ayushmondal001@gmail.com>
2025-03-23 00:15:24 +0530

    fix: build fix

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Test() {
   const [mainData, setMainData] = useState();
