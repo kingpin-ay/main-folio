9de7cc903745745b146d3a659c97b578190c7f68
kingpin-ay <ayushmondal001@gmail.com>
2025-03-29 19:32:51 +0530

    feat: added the cookie fix for the production

--- backend/lib/helper/cookie.ts
+++ backend/lib/helper/cookie.ts
@@ -12,6 +12,7 @@ export function cookieConfig(
     httpOnly: true,
     maxAge: 60 * 60 * maxAgeHour,
     expires: new Date(Date.now() + expiresDays * 24 * 60 * 60 * 1000),
-    sameSite: "none" as const,
+    sameSite:
+      env.DEV_ENV === "PRODUCTION" ? ("none" as const) : ("lax" as const),
   } as CookieOptions;
 }
